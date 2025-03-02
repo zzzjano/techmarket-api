46813c334ea0e9102b2a561967aef8ad8f040978
Jan Milewczyk <milewczykjan@gmail.com>
2025-03-02 17:44:03 +0100

    Skonczone zadanie 1

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,59 +1,75 @@
 let products = require('../data/products.js');
 const Product = require('../models/productModel.js');
 
-const getProducts = (req, res) => {
-    res.status(200).json(products);
+const getProducts = (req, res, next) => {
+    try {
+        res.status(200).json(products);
+    } catch (error) {
+        next(error);
+    }
 };
 
 const getProductById = (req, res, next) => {
-    const product = products.find((product) => product.id === parseInt(req.params.id));
-    if (product) {
-        res.status(200).json(product);
-    } else {
-        next(new Error('Product not found'));
+    try {
+        const product = products.find((product) => product.id === parseInt(req.params.id));
+        if (product) {
+            res.status(200).json(product);
+        } else {
+            throw new Error('Product not found');
+        }
+    } catch (error) {
+        next(error);
     }
 };
 
 const createProduct = (req, res, next) => {
-    try{
-    const product = req.body;
-    const newProduct = new Product(
-        product.id,
-        product.name,
-        product.category,
-        product.description,
-        product.price,
-        product.stockCount,
-        product.brand,
-        product.imageUrl,
-        product.isAvailable
-    );
+    try {
+        const product = req.body;
+        const newProduct = new Product(
+            product.id,
+            product.name,
+            product.category,
+            product.description,
+            product.price,
+            product.stockCount,
+            product.brand,
+            product.imageUrl,
+            product.isAvailable
+        );
         products.push(newProduct);
         res.status(201).json(newProduct);
-}catch(error){
-    next(error);
-}
+    } catch (error) {
+        next(error);
+    }
 }
 
 const updateProduct = (req, res, next) => {
-    const product = products.find((product) => product.id === parseInt(req.params.id));
-    if (product) {
-        const index = products.indexOf(product);
-        const updatedProduct = { ...product, ...req.body };
-        products[index] = updatedProduct;
-        res.status(200).json(updatedProduct);
-    } else {
-        next(new Error('Product not found'));
+    try {
+        const product = products.find((product) => product.id === parseInt(req.params.id));
+        if (product) {
+            const index = products.indexOf(product);
+            const updatedProduct = { ...product, ...req.body };
+            products[index] = updatedProduct;
+            res.status(200).json(updatedProduct);
+        } else {
+            throw new Error('Product not found');
+        }
+    } catch (error) {
+        next(error);
     }
 }
 
 const deleteProduct = (req, res, next) => {
-    const product = products.find((product) => product.id === parseInt(req.params.id));
-    if (product) {
-        products = products.filter((product) => product.id !== parseInt(req.params.id));
-        res.status(200).json({ message: 'Product removed' });
-    } else {
-        next(new Error('Product not found'));
+    try {
+        const product = products.find((product) => product.id === parseInt(req.params.id));
+        if (product) {
+            products = products.filter((product) => product.id !== parseInt(req.params.id));
+            res.status(200).json({ message: 'Product removed' });
+        } else {
+            throw new Error('Product not found');
+        }
+    } catch (error) {
+        next(error);
     }
 }
 
